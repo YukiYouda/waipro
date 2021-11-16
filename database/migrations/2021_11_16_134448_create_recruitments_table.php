75da9cd1ec38b3bcf3ceaf3becb2996448389d13
YukiYouda <youda0518@yahoo.co.jp>
2021-11-16 13:53:58 +0900

    Recruitmentモデル作成

---
+++ database/migrations/2021_11_16_134448_create_recruitments_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRecruitmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recruitments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->foreignId('category_id')
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->string('name');
+            $table->text('description');
+            $table->string('period');
+            $table->string('number');
+            $table->date('due_date');
+            $table->text('gain');
+            $table->text('caution');
+            $table->text('comment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recruitments');
+    }
+}
