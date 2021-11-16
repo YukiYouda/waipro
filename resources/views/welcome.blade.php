15d0ee2ef1d9049ad94af6c70e83c4cd163ad82e
YukiYouda <youda0518@yahoo.co.jp>
2021-11-16 20:00:55 +0900

    navigation-menu修正、welcomeページ修正

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -41,11 +41,11 @@
                                         転職希望の方はこちら
                                     </h3>
                                     <div>
-                                        {{-- <a href="{{ route('user.login') }}" class="w-full flex justify-center bg-gradient-to-r from-pink-500 to-purple-600 hover:bg-gradient-to-l hover:from-purple-500 hover:to-pink-600 text-gray-100 p-4 rounded-full tracking-wide font-semibold shadow-lg cursor-pointer transition ease-in duration-500">Sign in</a> --}}
+                                        <a href="{{ route('login') }}" class="w-full flex justify-center bg-gradient-to-r from-pink-500 to-purple-600 hover:bg-gradient-to-l hover:from-purple-500 hover:to-pink-600 text-gray-100 p-4 rounded-full tracking-wide font-semibold shadow-lg cursor-pointer transition ease-in duration-500">Sign in</a>
                                     </div>
                                     <p class="flex flex-col items-center justify-center mt-10 text-center text-md">
                                         <span>Don't have an account?</span>
-                                        {{-- <a href="{{ route('user.register') }}" --}}
+                                        <a href="{{ route('register') }}"
                                             class="hover:text-blue-500 no-underline hover:underline cursor-pointer transition ease-in duration-300">Sign
                                             up</a>
                                     </p>
