152bf2916e352164238a0a8fde8eeaa491956ebe
YukiYouda <youda0518@yahoo.co.jp>
2021-11-20 15:32:09 +0900

    recruitmentsとentriesのAPI完了

---
+++ app/Http/Controllers/Api/EntryController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Entry;
+use App\Models\Recruitment;
+
+class EntryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Recruitment $recruitment)
+    {
+        $entry = new Entry([
+            'recruitment_id' => $recruitment->id,
+            'user_id' => auth()->user()->id,
+        ]);
+
+        $entry->save();
+
+        return $entry;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Recruitment $recruitment, Entry $entry)
+    {
+        $entry->delete();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\RecruitmentController;
+use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\EntryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +21,25 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => ['api']], function(){
+
+    Route::get('recruitments/dashboard', [RecruitmentController::class, 'dashboard'])
+    ->middleware('auth:sanctum')
+    ->name('recruitments.dashboard');
+
+    Route::apiResource('recruitments', RecruitmentController::class)
+        ->middleware('auth:sanctum');
+    
+    Route::apiResource('recruitments.entries', EntryController::class)
+        ->only(['store', 'destroy'])
+        ->middleware('auth:sanctum');
+    
+    
+});
+
+// ユーザー登録
+Route::post('/register', [RegisterController::class, 'register']);
+
+// ログイン
+Route::post('/login', [LoginController::class, 'login']);
